207e1df931a37b50473fefcedde1005fc145b267
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-01-06 22:09:08 +0530

    Time: 332 ms (5.04%), Space: 45.4 MB (5.03%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,37 +1,20 @@
-// class Solution {
-//     public int[] minOperations(String boxes) {
-//         int n=boxes.length();
-//         int[] ans;
-//         int[] Box = new int[n];
-//         for(int i=0;i<n;i++){
-//             Box[i]+=boxes.charAt(i)-'0';
-//         }
-//         int[] ans = new int[s.length()];
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++){
-//                 if(Box[i]==1){
-//                     ans[j]=abs(Box[i] - j);
-//                 }
-//             }
-//         }
-
-//         } 
-//     } 
-
-
 class Solution {
     public int[] minOperations(String boxes) {
-        int[] ans = new int[boxes.length()];
-        int total = 0;
-        for (int i = 0; i < boxes.length(); i++) {
-            total = 0;
-            for (int j = 0; j < boxes.length(); j++) {
-                if (i == j)
-                    continue;
-                total += Math.abs(j - i) * Character.getNumericValue(boxes.charAt(j));
+        int n = boxes.length();
+        int[] ans = new int[n];
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for(int i = 0; i < n; i++) {
+            if(boxes.charAt(i) == '1') {
+                hs.add(i);
             }
-            ans[i] = total;
         }
-        return ans;
+
+        for(int i = 0; i < n; i++) {
+            for(int idx : hs) {
+                ans[i] += Math.abs(idx-i);
+            }
+        }
+        return ans;        
     }
 }
