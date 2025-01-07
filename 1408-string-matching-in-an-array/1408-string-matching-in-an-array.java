732a7d1cdeeaacac992a9e55ad4437109ad09162
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-01-07 10:02:35 +0530

    Time: 5 ms (41.61%), Space: 42.1 MB (73.02%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,3 +1,21 @@
+// class Solution {
+//     public List<String> stringMatching(String[] words) {
+//         ArrayList<String> ans = new ArrayList<String>();
+//         int n = words.length;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i!=j && words[j].contains(words[i])){
+//                     ans.add(words[i]);
+//                 }
+//             }
+            
+//         }
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public List<String> stringMatching(String[] words) {
         ArrayList<String> ans = new ArrayList<String>();
@@ -5,7 +23,10 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j && words[j].contains(words[i])){
-                    ans.add(words[i]);
+                    if(ans.contains(words[i])==false){
+                               ans.add(words[i]);
+                    }
+                  
                 }
             }
             
