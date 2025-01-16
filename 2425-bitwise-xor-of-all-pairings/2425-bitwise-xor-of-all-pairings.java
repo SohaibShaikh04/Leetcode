553e32db1497925dc55343125e9cb0d73b66a5ed
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-01-16 15:03:43 +0530

    Time: 1 ms (100%), Space: 61.3 MB (68.97%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.java
@@ -1,21 +1,21 @@
 class Solution {
     public int xorAllNums(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-       Map<Integer,Integer> map = new HashMap<>();
+        int xor = 0;
 
-       for(int num : nums1){
-        map.put(num,map.getOrDefault(num,0)+m);
-       }
-       for(int num : nums2){
-        map.put(num,map.getOrDefault(num,0)+n);
-       }
-       int result=0;
-       for(int num : map.keySet()){
-          if(map.get(num) % 2 != 0){
-            result^=num;
-          }
-       }
-       return result;
+       
+        if (nums2.length % 2 != 0) {
+            for (int num : nums1) {
+                xor ^= num;
+            }
+        }
+
+        
+        if (nums1.length % 2 != 0) {
+            for (int num : nums2) {
+                xor ^= num;
+            }
+        }
+
+        return xor;
     }
 }
