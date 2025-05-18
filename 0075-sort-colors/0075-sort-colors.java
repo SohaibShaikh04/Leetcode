26e12fe0f402f0ae811b890f648ea3c5ad5a4e61
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-05-19 00:04:40 +0530

    Time: 8 ms (3.08%), Space: 42.3 MB (9.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,1 +1,9 @@
-nums
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        for(int x : nums){
+            System.out.println(x);
+        }
+      
+    }
+}
