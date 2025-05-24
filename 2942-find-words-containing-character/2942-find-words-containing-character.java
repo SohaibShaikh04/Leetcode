743ead1ad24d331d95f382493621cd4581d66159
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-05-24 12:08:13 +0530

    Time: 2 ms (52.59%), Space: 45 MB (59.58%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = words.length;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<words[i].length();j++)
+            {
+                if(words[i].charAt(j)==x)
+                {
+                 list.add(i);
+                 break;
+                }
+            }
+
+        }
+        return list;
+    }
+}
+
+// since list use karr rhe toh duplicates bhi aajaenge par apne ko ek baar hi count lena hai toh jaisi woh update ho break kardenge
