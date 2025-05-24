c1006dd6c66d126472a9672f511bfa1f9f844b2a
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-05-24 12:17:49 +0530

    Time: 1 ms (100%), Space: 44.9 MB (76.79%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.java
+++ 2942-find-words-containing-character/2942-find-words-containing-character.java
@@ -1,21 +1,16 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int n = words.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<words[i].length();j++)
-            {
-                if(words[i].charAt(j)==x)
-                {
-                 list.add(i);
-                 break; //To avoid duplicates
-                }
-            }
-
+     ArrayList<Integer> list = new ArrayList<>();
+     int n=words.length;
+     for(int i=0;i<n;i++){
+        if(words[i].indexOf(x)!=-1){
+            list.add(i);
         }
-        return list;
+     }
+        return list ;
     }
 }
 
-// since list use karr rhe toh duplicates bhi aajaenge par apne ko ek baar hi count lena hai toh jaisi woh update ho break kardenge
+
+//learnt about a new function called "indexOf()" -> helps to find the first occurence of  a word in a string .....//
+//this makes the time complexity to O(n);
