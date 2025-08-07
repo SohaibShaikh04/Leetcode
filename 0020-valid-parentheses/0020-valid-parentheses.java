4ca80511924dc6793595314b0deace88df371520
Sohaib Shaikh <sohaibsk2004@gmail.com>
2025-08-07 22:56:14 +0530

    Time: 2 ms (96.58%), Space: 41.5 MB (97.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='(' || ch=='{' || ch=='['){
+        for(char ch : s.toCharArray()) {
+            if(ch == '(' || ch == '{' || ch == '[') {
                 st.push(ch);
-            }
-            else{
-                if(st.isEmpty())return false;
-                if(ch==')'){
-                    char poppedCh=st.pop();
-                    if(poppedCh!='(') return false;
-                }
-                if(ch=='}'){
-                    char poppedCh=st.pop();
-                    if(poppedCh!='{') return false;
-                }
-                if(ch==']'){
-                    char poppedCh=st.pop();
-                    if(poppedCh!='[') return false;
-                }
-
+            } else {
+                if(st.isEmpty()) return false;
+                
+                char top = st.pop();
+                
+                if((ch == ')' && top != '(') ||
+                   (ch == '}' && top != '{') ||
+                   (ch == ']' && top != '[')) {
+                    return false;
                 }
             }
-            return st.size()==0;
         }
+        return st.isEmpty();
+    }
 }
-        
