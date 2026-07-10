24ba3109376804d70d3842e81ce1b72738682ba6
Sohaib Shaikh <sohaibsk2004@gmail.com>
2026-07-10 22:51:53 +0530

    Time: 33 ms (74.01%), Space: 58.9 MB (85.36%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,36 @@
-
-
-
 class Solution {
-    
     public List<List<Integer>> threeSum(int[] nums) {
-         List<List<Integer>> list = new ArrayList<>();
-         Arrays.sort(nums);
-         for(int i=0;i<nums.length - 2;i++){
-            int left = i+1;
-            int right =  nums.length - 1;
+        Arrays.sort(nums);
+        List<List<Integer>> list= new ArrayList<>();
+        for(int i=0;i<nums.length - 2;i++){
+            int left = i + 1;
+            int right = nums.length - 1;
 
-            if(i > 0 && nums[i] == nums[i-1]) continue;
-
-            
-            while(right >left){
-                if(nums[left] + nums[right] == - (nums[i])){
-                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            while(left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+               
+                if(sum == 0){
+                    list.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
                     left++;
-                    
-                  while (nums[left] == nums[left-1] && left < right) {
-                        left++;
-                    }
+                     while(left < right && nums[left] == nums[left-1]){
+                      left++;
+                     }
+                     right--;
+                     while(right >0 && nums[right-1] == nums[right+1]){
+                     right--;
                 }
-                else if(nums[left] + nums[right] > - (nums[i])){
-                    right--;
+                }
+                else if(sum<0){
+                    left++;
                 }
                 else{
-                    left++;
+                    right --;
                 }
+               
+
             }
-         }
-         return list;
+        }
+        return list;
     }
 }
