9aadbe1860378b221d22b58948b37da05c86e507
Sohaib Shaikh <sohaibsk2004@gmail.com>
2026-07-27 14:46:21 +0530

    Time: 1 ms (66.2%), Space: 44.4 MB (73.61%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,8 +1,26 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        int prod = (nums[n-1] - 1) * (nums[n-2] - 1 );
-        return prod;
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE ;
+        int largeIdx=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] > largest){
+                largest = nums[i];
+                largeIdx = i;
+            }
+        }
+
+        for(int i =0;i<n;i++){
+             if(nums[i] > secondLargest && secondLargest <= largest && i != largeIdx ){
+                secondLargest = nums[i];
+            }
+        }
+
+        int prod =  (largest - 1) * (secondLargest - 1);
+        
+       return prod;
     }
 }
+
+// Previous waala approach was o(n^2) sorting karne ke wajah se this one is just basic largest and second largest ;) sohaib..
