6da1565b8ff0bc7ad683a9da04dc77db7939cabb
Sohaib Shaikh <sohaibsk2004@gmail.com>
2026-08-21 16:00:16 +0530

    Time: 1 ms (95.86%), Space: 43.6 MB (71.98%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.java
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.java
@@ -2,27 +2,32 @@ class Solution {
     public boolean equationsPossible(String[] equations) {
         int[] parent = new int[26];
         int[] rank = new int[26];
-        Arrays.fill(rank,0);
+
         for(int i=0;i<26;i++){
             parent[i] = i;
+            rank[i] = 0;
         }
 
-       for(String s : equations) {
-            if(s.charAt(1) == '=')
-                Union(s.charAt(0)-'a', s.charAt(3)-'a',parent , rank);
+        for(String s : equations){
+            if(s.charAt(1) == '='){
+                Union(s.charAt(0)-'a' , s.charAt(3)-'a' , parent , rank);
+            }
         }
-        
-        for(String s : equations) {
-            if(s.charAt(1) == '!') {
-                if(find(s.charAt(0)-'a',parent) == find(s.charAt(3)-'a',parent))
+
+        for(String s : equations){
+            if(s.charAt(1) == '!'){
+                int x_p = find(s.charAt(0)-'a' , parent);
+                int y_p = find(s.charAt(3)-'a', parent );
+
+                if(x_p == y_p) {
                     return false;
+                }
             }
         }
         return true;
+    }
 
-        }
-
-    public int find(int x , int[] parent ){
+    public int find(int x , int[] parent){
         if(x == parent[x]){
             return x;
         }
@@ -30,23 +35,22 @@ class Solution {
     }
 
     public void Union(int x , int y , int[] parent , int[] rank){
-        int x_parent = find(x,parent);
-        int y_parent = find(y,parent);
-
+        int x_parent = find(x , parent);
+        int y_parent = find(y , parent);
         if(x_parent == y_parent){
-            return;
+            return ;
         }
-        
-            if(rank[x_parent] > rank[y_parent]){
-                parent[y_parent] = x_parent;
-            }
-            else if(rank[x_parent] < rank[y_parent]){
-                parent[x_parent] = y_parent;
-            }
-            else{
-                parent[y_parent] = x_parent;
-                rank[x_parent]++;
-            }
-        
+
+        if(rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if(rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else{
+            parent[y_parent] = x_parent;
+            rank[x_parent]++;
+        }
+
     }
 }
