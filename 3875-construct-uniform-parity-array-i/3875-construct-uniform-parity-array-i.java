da931582544ee16e43ebc49a685617f3a5676c0a
Sohaib Shaikh <sohaibsk2004@gmail.com>
2026-09-02 21:36:10 +0530

    Time: 1 ms (31.89%), Space: 45.4 MB (5.31%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,6 +1,20 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        return true;
+        int n = nums1.length;
+        int[] nums2 = new int[n];
+
+        if(nums1.length == 1) return true;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<n;j++){
+                nums2[i] = nums1[i];
+                if(i != j){
+                    if(nums2[i] == nums1[i] || nums2[i] == nums1[i] - nums1[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
-// ) :
+// ab kar liya bhai o(n^2) se --Sohaib
