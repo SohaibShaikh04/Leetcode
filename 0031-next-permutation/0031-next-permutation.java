7da5146f7255059e0e25c7e00dd43dc026c9e508
Sohaib Shaikh <sohaibsk2004@gmail.com>
2026-09-18 23:48:47 +0530

    Time: 0 ms (100%), Space: 44.7 MB (32.82%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public void reverse(int[] nums , int left , int right){
+        while(right > left){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int pivotIndex = -1;
+        //iterate backwords and find the element which is smaller then its next index find pivot 
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i] < nums[i+1]){
+                pivotIndex = i;
+                break;
+            }
+        }
+        // if idx remains -1
+        if(pivotIndex == -1){
+            reverse(nums,0,n-1);
+            return;
+        }
+
+        //once pivot is reached iterate again till pivotIndex and try to swap the element greater than the pivot index
+        for(int i=n-1;i>pivotIndex;i--){
+            if(nums[i] > nums[pivotIndex]){
+                int temp = nums[i];
+                nums[i] = nums[pivotIndex];
+                nums[pivotIndex] =temp;
+                break;
+            }
+        }
+        //once swapping is done reverse the array from the pivot position
+        reverse(nums , pivotIndex+1 , n-1);
+        
+    }
+}
